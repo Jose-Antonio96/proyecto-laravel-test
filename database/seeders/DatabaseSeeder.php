064d9db4282ff936d54d49f077c49311cbc006b6
JoseAPR <92516246+Jose-Antonio96@users.noreply.github.com>
2023-01-18 17:54:26 +0100

    Small changes in models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,6 +20,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-       $this->call(ArticuloSeeder::class);
+       $admin = new User();
+       $admin -> name = "Superadmin";
+       $admin ->email = "admin@miappcom";
+       $admin ->password = "woahsafe";
+       $admin->save();
+        
+       User::create(['name' => 'Test User',
+        'email' => 'test',
+        'password' => 'passtest'  
+        ]);
     }
 }
