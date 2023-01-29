3cee69a4161cd9dc2aef6f4e3a43744ee3db7bb7
JoseAPR <92516246+Jose-Antonio96@users.noreply.github.com>
2023-01-29 21:10:05 +0100

    avances en footer y header

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang=”es”>
 <head>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<meta charset=”UTF-8”>
 	<meta name viewport content=”width-device-width, initial-scale-1.0”>
 	<title>Traveliens - {{$title ?? '404'}}</title>
@@ -9,11 +12,15 @@
 </head>
 <body>
 	{{--@include('partials.navigation')--}}
-        
+	
+<x-layouts.header/>      
 <x-layouts.navigation />
 
 
 
 {{$slot}}
 </body>
+<footer>	
+	<x-layouts.footer/>
+</footer>
 </html>
